47f6c51b1690b6bfab1a9c29bb1c30ad4144d95e
Fernando Pereira Xavier <fernandoxavier233@gmail.com>
2022-05-10 22:09:42 -0300

    update

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,15 +5,25 @@ import dotenv from 'dotenv';
 dotenv.config() 
 
 export const connection = new Sequelize(
-    process.env.DB_BASE,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_LOCAL,
-        port: 5432,
-        dialect: "postgres"
-    },
-    'postgres://catalogo_livros_user:qAHH3I87YSTEk3VqAUDHb3HQmLRhcggz@dpg-c9tg4uj19n07s441pv7g-a.oregon-postgres.render.com/catalogo_livros',
+    // process.env.DB_BASE,
+    // process.env.DB_USER,
+    // process.env.DB_PASS,
+    // {
+    //     host: process.env.DB_LOCAL,
+    //     port: 5432,
+    //     dialect: "postgres"
+    // },
+    // 'postgres://catalogo_livros_user:qAHH3I87YSTEk3VqAUDHb3HQmLRhcggz@dpg-c9tg4uj19n07s441pv7g-a.oregon-postgres.render.com/catalogo_livros',
+    // {
+    //     dialect: 'postgres',
+    //     dialectOptions: {
+    //         ssl: {
+    //             require: true,
+    //             rejectUnauthorized: false
+    //         }
+    //     }
+    // }
+    process.env.DB_URL,
     {
         dialect: 'postgres',
         dialectOptions: {
@@ -23,4 +33,7 @@ export const connection = new Sequelize(
             }
         }
     }
-)
+       
+    );
+
+
